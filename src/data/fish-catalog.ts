52d1fa5833b204eba717d288b885842a5f5e0cdf
PCastro2001 <87351468+PCastro2001@users.noreply.github.com>
2026-08-23 15:29:50 -0400

    feat: import searchable fish catalog

---
+++ src/data/fish-catalog.ts
@@ -0,0 +1,767 @@
+export interface FishRecord {
+  name: string;
+  habitat: string;
+  waterType: string;
+  weather: string;
+  timeOfDay: string;
+}
+
+/** Normal-size fish records imported from the public TRUE Perfection Tracker. */
+export const FISH_CATALOG: readonly FishRecord[] = [
+  {
+    "name": "Bream",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Day"
+  },
+  {
+    "name": "Roach",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Evening"
+  },
+  {
+    "name": "Goldfish",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Clear",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Mackerel",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Foggy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Needlefish",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Stormy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Carp",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Morning"
+  },
+  {
+    "name": "Black Sharkminnow",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Boxfish",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Gulper Eel",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "John Dory",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Lionfish",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "Northern Pufferfish",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Day"
+  },
+  {
+    "name": "Red Melon Discus",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Foggy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Alligator Gar",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Stormy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Salween Rita Catfish",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "European Anglerfish",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "Hogfish",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Goliath Tigerfish",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Frilled Shark",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Blind Bladefish",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Red Dartfin",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Spineback Ray",
+    "habitat": "Coconut Bay",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Dragonfly Fish",
+    "habitat": "Coconut Bay",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Ocean Perch",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Stormy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Tarpon",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Foggy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Black Scorpionfish",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Evening"
+  },
+  {
+    "name": "Snook",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Rainy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Lionfish",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "Clownfish",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Day"
+  },
+  {
+    "name": "Regal Blue Tang",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Morning"
+  },
+  {
+    "name": "Parrotfish",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Clear",
+    "timeOfDay": "Day"
+  },
+  {
+    "name": "Permit",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Stormy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Hammerhead Shark",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Evening"
+  },
+  {
+    "name": "Brickfish",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Armored Brutefish",
+    "habitat": "Crescent Isle",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Celestial Whitefin",
+    "habitat": "Crescent Isle",
+    "waterType": "Any",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Flame Guppy",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Magma Carp",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Ashscale Trout",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Basalt Eel",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Cinderfin",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Obsidian Fish",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Crystal Pike",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Molten Angler",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Ifrit Barracuda",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Pyrite Snapper",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Igneous Stingray",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Red Demonfish",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Hellmaw Grouper",
+    "habitat": "Volcanic Depths",
+    "waterType": "Lava",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Bluegill Sunfish",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Clear",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Bowfin",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Any",
+    "timeOfDay": "Evening"
+  },
+  {
+    "name": "Channel Catfish",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Foggy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Cottonmouth Snake",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Clear",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Frog",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Rainy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Alligator Snapping Turtle",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "American Alligator",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "Giant Gharial",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Venomous Watcher",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Dreadshell Colossus",
+    "habitat": "Tanglewood",
+    "waterType": "Swampwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Ide",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Morning"
+  },
+  {
+    "name": "Tench",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "Perch",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Stormy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "GiltHead Bream",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Clear",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Tilefish",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Rainbow Trout",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Foggy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Eel",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "Bluefish",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Rainy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Hawaiian Triggerfish",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "John Dory",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Barracuda",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Morning"
+  },
+  {
+    "name": "Snow Yellow Discus",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Foggy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Amberjack",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Goonch Catfish",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Rainy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Siamese Giant Carp",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Morning"
+  },
+  {
+    "name": "Wahoo",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Ironfin Stalker",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Pinnate Batfish",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Ancient Warriorfish",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Humpback Gar",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Shellonodon",
+    "habitat": "Luxian Dunes",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Royal Bananafish",
+    "habitat": "Luxian Dunes",
+    "waterType": "Freshwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Herring",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Haddock",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Cod",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Morning"
+  },
+  {
+    "name": "Atlantic Salmon",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Evening"
+  },
+  {
+    "name": "Pollock",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Rainy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Halibut",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Stormy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Crab",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "Mahi-Mahi",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Day"
+  },
+  {
+    "name": "Flying Fish",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Clear",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Albacore Tuna",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Rainy",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Sailfish",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Morning"
+  },
+  {
+    "name": "Blobfish",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Morning"
+  },
+  {
+    "name": "Common Stingray",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "Seahorse",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Rainy",
+    "timeOfDay": "Evening"
+  },
+  {
+    "name": "Sunfish",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Day"
+  },
+  {
+    "name": "Oarfish",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Manta Ray",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Day"
+  },
+  {
+    "name": "Great White Shark",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Giant Squid",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Abyssal Serpentfish",
+    "habitat": "Open Sea",
+    "waterType": "Salt & Swamp",
+    "weather": "Any",
+    "timeOfDay": "Night"
+  },
+  {
+    "name": "Baby Megalodon",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  },
+  {
+    "name": "Three-Headed Salmon",
+    "habitat": "Open Sea",
+    "waterType": "Saltwater",
+    "weather": "Any",
+    "timeOfDay": "Any"
+  }
+] as const;
+
+/** Tracker mutations. Tiny and Huge are size variants, not mutations. */
+export const MUTATION_NAMES = [
+  "Regular",
+  "Albino",
+  "Blessed",
+  "Burning",
+  "Camo",
+  "Cursed",
+  "Electric",
+  "Frozen",
+  "Galaxy",
+  "Ghastly",
+  "Glitched",
+  "Golden",
+  "Holographic",
+  "Negative",
+  "Radioactive",
+  "Rainbow",
+  "Sandy",
+  "Shadow",
+  "Shiny",
+  "Static",
+  "Stone",
+  "Tiger",
+  "Void",
+  "Zebra"
+] as const;
+
+export const FISH_DATA_COVERAGE = {
+  source: "FISH! Index TRUE Perfection Tracker by LucyferVC (Public Copy 2.0)",
+  habitats: 6,
+  records: FISH_CATALOG.length,
+  missingHabitats: ["Twilight Realms"],
+} as const;
