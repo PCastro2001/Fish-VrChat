3a6162b0a3b853b410c915d2a3f00b42bfa3c55b
PCastro2001 <chicastro2001@gmail.com>
2026-08-24 00:37:17 -0400

    Actualizacion

--- fish-vrchat-wiki/src/data/gear.ts
+++ fish-vrchat-wiki/src/data/gear.ts
@@ -1,57 +1,88 @@
 export interface GearItem {
     name: string;
-    photo: string;
     price: string;
+    costNum: number;
     location: string;
     luck: string;
     strength: string;
     expertise: string;
     attractionRate: string;
     bigCatchRate: string;
-    maxWeight?: string; // Solo aplica para Rods
+    maxWeight: string;
+}
+
+export interface EnchantmentItem {
+    name: string;
+    rarity: string;
+    costNum: number;
+    luck: string;
+    strength: string;
+    expertise: string;
+    attractionRate: string;
+    bigCatchRate: string;
+    maxWeight: string;
+    description: string;
 }
 
 export const rodsData: GearItem[] = [
-    { name: "Stick and String", photo: "/images/rods/stick_and_string.png", price: "$0", location: "Default Rod", luck: "-50", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-100", maxWeight: "5kg" },
-    { name: "Sunleaf Rod", photo: "/images/rods/sunleaf_rod.png", price: "$0", location: "Sell Shops", luck: "+10", strength: "+5", expertise: "+10", attractionRate: "+25%", bigCatchRate: "+15", maxWeight: "250kg" },
-    { name: "Toy Rod", photo: "/images/rods/toy_rod.png", price: "$750", location: "Coconut Bay", luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "15kg" },
-    { name: "Sturdy Wooden Rod", photo: "/images/rods/sturdy_wooden_rod.png", price: "$2,000", location: "Coconut Bay", luck: "+15", strength: "-", expertise: "+5", attractionRate: "+20%", bigCatchRate: "-", maxWeight: "30kg" },
-    { name: "Slim Rod", photo: "/images/rods/slim_rod.png", price: "$10,000", location: "Coconut Bay", luck: "+20", strength: "+10", expertise: "+10", attractionRate: "+25%", bigCatchRate: "+20", maxWeight: "500kg" },
-    { name: "Telescopic Rod", photo: "/images/rods/telescopic_rod.png", price: "$15,000", location: "Coconut Bay", luck: "+10", strength: "+15", expertise: "+15", attractionRate: "+10%", bigCatchRate: "+5", maxWeight: "2,005kg" },
-    { name: "Metallic Rod", photo: "/images/rods/metallic_rod.png", price: "$15,000", location: "Coconut Bay", luck: "-", strength: "+55", expertise: "+55", attractionRate: "+10%", bigCatchRate: "+10", maxWeight: "1,000kg" },
-    { name: "Darkwood Rod", photo: "/images/rods/darkwood_rod.png", price: "$25,000", location: "Tanglewood", luck: "+30", strength: "+10", expertise: "+10", attractionRate: "+30%", bigCatchRate: "+5", maxWeight: "1,800kg" },
-    { name: "Speedy Rod", photo: "/images/rods/speedy_rod.png", price: "$55,000", location: "Crescent Isle", luck: "+1", strength: "+5", expertise: "+5", attractionRate: "+60%", bigCatchRate: "-", maxWeight: "1500kg" },
-    { name: "Fortunate Rod", photo: "/images/rods/fortunate_rod.png", price: "$75,000", location: "Crescent Isle", luck: "+100", strength: "+10", expertise: "+5", attractionRate: "+10%", bigCatchRate: "+65", maxWeight: "1500kg" },
-    { name: "Alien Rod", photo: "/images/rods/alien_rod.png", price: "$200,000", location: "Alien Quest", luck: "+55", strength: "+10", expertise: "+10", attractionRate: "+45%", bigCatchRate: "+30", maxWeight: "32,000kg" },
-    { name: "Rustfang Rod", photo: "/images/rods/rustfang_rod.png", price: "$250,000", location: "Luxian Dunes", luck: "+70", strength: "+20", expertise: "+20", attractionRate: "+25%", bigCatchRate: "+35", maxWeight: "35,000kg" },
-    { name: "Runesteel Rod", photo: "/images/rods/runesteel_rod.png", price: "$700,000", location: "Vlad (Islander)", luck: "+90", strength: "+25", expertise: "+20", attractionRate: "+30%", bigCatchRate: "+40", maxWeight: "100,000kg" },
-    { name: "Rod of the Pharaoh", photo: "/images/rods/rod_of_the_pharaoh.png", price: "$750,000", location: "Luxian Dunes", luck: "+222", strength: "+20", expertise: "+40", attractionRate: "-10%", bigCatchRate: "+35", maxWeight: "100,000kg" },
-    { name: "Rod of Perpetuity", photo: "/images/rods/rod_of_perpetuity.png", price: "$0", location: "Reach level 500", luck: "+150", strength: "+30", expertise: "+30", attractionRate: "+50%", bigCatchRate: "+10", maxWeight: "500,000kg" },
-    { name: "Leviathan Rod", photo: "/images/rods/leviathan_rod.png", price: "$1,000,000", location: "Crafted with Tomina's Crafting Services at Crescent Isle", luck: "+500", strength: "+20", expertise: "+20", attractionRate: "+25%", bigCatchRate: "+35", maxWeight: "100,000kg" }
+    { name: "Stick and String", price: "$0", costNum: 0, location: "Default Rod", luck: "-50", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-100", maxWeight: "5kg" },
+    { name: "Sunleaf Rod", price: "$0", costNum: 0, location: "Sell Shops", luck: "+10", strength: "+5", expertise: "+10", attractionRate: "+25%", bigCatchRate: "+15", maxWeight: "250kg" },
+    { name: "Toy Rod", price: "$750", costNum: 750, location: "Coconut Bay", luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "15kg" },
+    { name: "Sturdy Wooden Rod", price: "$2,000", costNum: 2000, location: "Coconut Bay", luck: "+15", strength: "-", expertise: "+5", attractionRate: "+20%", bigCatchRate: "-", maxWeight: "30kg" },
+    { name: "Slim Rod", price: "$10,000", costNum: 10000, location: "Coconut Bay", luck: "+20", strength: "+10", expertise: "+10", attractionRate: "+25%", bigCatchRate: "+20", maxWeight: "500kg" },
+    { name: "Telescopic Rod", price: "$15,000", costNum: 15000, location: "Coconut Bay", luck: "+10", strength: "+15", expertise: "+15", attractionRate: "+10%", bigCatchRate: "+5", maxWeight: "2,005kg" },
+    { name: "Metallic Rod", price: "$15,000", costNum: 15000, location: "Coconut Bay", luck: "-", strength: "+55", expertise: "+55", attractionRate: "+10%", bigCatchRate: "+10", maxWeight: "1,000kg" },
+    { name: "Darkwood Rod", price: "$25,000", costNum: 25000, location: "Tanglewood", luck: "+30", strength: "+10", expertise: "+10", attractionRate: "+30%", bigCatchRate: "+5", maxWeight: "1,800kg" },
+    { name: "Speedy Rod", price: "$55,000", costNum: 55000, location: "Crescent Isle", luck: "+1", strength: "+5", expertise: "+5", attractionRate: "+60%", bigCatchRate: "-", maxWeight: "1,500kg" },
+    { name: "Fortunate Rod", price: "$75,000", costNum: 75000, location: "Crescent Isle", luck: "+100", strength: "+10", expertise: "+5", attractionRate: "+10%", bigCatchRate: "+65", maxWeight: "1,500kg" },
+    { name: "Alien Rod", price: "$200,000", costNum: 200000, location: "Alien Quest", luck: "+55", strength: "+10", expertise: "+10", attractionRate: "+45%", bigCatchRate: "+30", maxWeight: "32,000kg" },
+    { name: "Rustfang Rod", price: "$250,000", costNum: 250000, location: "Luxian Dunes", luck: "+70", strength: "+20", expertise: "+20", attractionRate: "+25%", bigCatchRate: "+35", maxWeight: "35,000kg" },
+    { name: "Runesteel Rod", price: "$700,000", costNum: 700000, location: "Vlad (Islander)", luck: "+90", strength: "+25", expertise: "+20", attractionRate: "+30%", bigCatchRate: "+40", maxWeight: "100,000kg" },
+    { name: "Rod of the Pharaoh", price: "$750,000", costNum: 750000, location: "Luxian Dunes", luck: "+222", strength: "+20", expertise: "+40", attractionRate: "-10%", bigCatchRate: "+35", maxWeight: "100,000kg" },
+    { name: "Rod of Perpetuity", price: "$0", costNum: 0, location: "Reach level 500", luck: "+150", strength: "+30", expertise: "+30", attractionRate: "+50%", bigCatchRate: "+10", maxWeight: "500,000kg" },
+    { name: "Leviathan Rod", price: "$1,000,000", costNum: 1000000, location: "Crafted at Crescent Isle", luck: "+500", strength: "+20", expertise: "+20", attractionRate: "+25%", bigCatchRate: "+35", maxWeight: "100,000kg" }
 ];
 
 export const linesData: GearItem[] = [
-    { name: "Basic Line", photo: "/images/lines/basic_line.png", price: "$0", location: "Default Line", luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-" },
-    { name: "Aquamarine Line", photo: "/images/lines/aquamarine_line.png", price: "$100", location: "Coconut Bay", luck: "-", strength: "-", expertise: "-", attractionRate: "+5%", bigCatchRate: "-" },
-    { name: "Carbon Line", photo: "/images/lines/carbon_line.png", price: "$1,000", location: "Coconut Bay", luck: "-", strength: "+7", expertise: "+7", attractionRate: "-", bigCatchRate: "-" },
-    { name: "Heavy Duty Line", photo: "/images/lines/heavy_duty_line.png", price: "$4,000", location: "Crescent Isle", luck: "-", strength: "+10", expertise: "+10", attractionRate: "-", bigCatchRate: "+10" },
-    { name: "Flavored Line", photo: "/images/lines/flavored_line.png", price: "$10,000", location: "Tanglewood", luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "+30" },
-    { name: "Lucky Line", photo: "/images/lines/lucky_line.png", price: "$10,000", location: "Crescent Isle", luck: "+30", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-" },
-    { name: "Diamond Line", photo: "/images/lines/diamond_line.png", price: "$25,000", location: "Luxian Dunes", luck: "+25", strength: "+15", expertise: "+15", attractionRate: "+10%", bigCatchRate: "-" },
-    { name: "Fur of Cerberus", photo: "/images/lines/fur_of_cerberus.png", price: "$35,000", location: "Vlad (Islander)", luck: "+25", strength: "-5", expertise: "-15", attractionRate: "+20%", bigCatchRate: "+10" },
-    { name: "Midas Line", photo: "/images/lines/midas_line.png", price: "$50,000", location: "Twilight Realm", luck: "+60", strength: "+15", expertise: "+10", attractionRate: "+15%", bigCatchRate: "+5" },
-    { name: "Ethereal Line", photo: "/images/lines/ethereal_line.png", price: "$0", location: "Complete the Quest: Our Ship... It's Broken!", luck: "+75", strength: "-5", expertise: "-15", attractionRate: "+20%", bigCatchRate: "+20" }
+    { name: "Basic Line", price: "$0", costNum: 0, location: "Default Line", luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Aquamarine Line", price: "$100", costNum: 100, location: "Coconut Bay", luck: "-", strength: "-", expertise: "-", attractionRate: "+5%", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Carbon Line", price: "$1,000", costNum: 1000, location: "Coconut Bay", luck: "-", strength: "+7", expertise: "+7", attractionRate: "-", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Heavy Duty Line", price: "$4,000", costNum: 4000, location: "Crescent Isle", luck: "-", strength: "+10", expertise: "+10", attractionRate: "-", bigCatchRate: "+10", maxWeight: "-" },
+    { name: "Flavored Line", price: "$10,000", costNum: 10000, location: "Tanglewood", luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "+30", maxWeight: "-" },
+    { name: "Lucky Line", price: "$10,000", costNum: 10000, location: "Crescent Isle", luck: "+30", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Diamond Line", price: "$25,000", costNum: 25000, location: "Luxian Dunes", luck: "+25", strength: "+15", expertise: "+15", attractionRate: "+10%", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Fur of Cerberus", price: "$35,000", costNum: 35000, location: "Vlad (Islander)", luck: "+25", strength: "-5", expertise: "-15", attractionRate: "+20%", bigCatchRate: "+10", maxWeight: "-" },
+    { name: "Midas Line", price: "$50,000", costNum: 50000, location: "Twilight Realm", luck: "+60", strength: "+15", expertise: "+10", attractionRate: "+15%", bigCatchRate: "+5", maxWeight: "-" },
+    { name: "Ethereal Line", price: "$0", costNum: 0, location: "Quest: Our Ship...", luck: "+75", strength: "-5", expertise: "-15", attractionRate: "+20%", bigCatchRate: "+20", maxWeight: "-" }
 ];
 
 export const bobbersData: GearItem[] = [
-    { name: "Basic Bobber", photo: "/images/bobbers/basic_bobber.png", price: "$0", location: "Default Bobber", luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-" },
-    { name: "Paulie's Bobber", photo: "/images/bobbers/paulies_bobber.png", price: "$0", location: "Complete the Quest: Paulie's Lost Saw", luck: "-", strength: "-", expertise: "+5", attractionRate: "+5%", bigCatchRate: "-" },
-    { name: "Blue Bobber", photo: "/images/bobbers/blue_bobber.png", price: "$100", location: "Coconut Bay", luck: "+5", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-" },
-    { name: "Dud Bobber", photo: "/images/bobbers/dud_bobber.png", price: "$1,000", location: "Crescent Isle", luck: "+5", strength: "-", expertise: "+5", attractionRate: "-", bigCatchRate: "-" },
-    { name: "Feline Bobber", photo: "/images/bobbers/feline_bobber.png", price: "$2,000", location: "Tanglewood", luck: "+5", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "+10" },
-    { name: "Ornamental Bobber", photo: "/images/bobbers/ornamental_bobber.png", price: "$10,000", location: "Luxian Dunes", luck: "+10", strength: "+5", expertise: "-", attractionRate: "+10%", bigCatchRate: "-" },
-    { name: "Lucky Bobber", photo: "/images/bobbers/lucky_bobber.png", price: "$10,000", location: "Luxian Dunes", luck: "+40", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-" },
-    { name: "Rainbow Slime Bobber", photo: "/images/bobbers/rainbow_slime_bobber.png", price: "$35,000", location: "Vlad (Islander)", luck: "+30", strength: "+10", expertise: "-", attractionRate: "+10%", bigCatchRate: "+10" },
-    { name: "Pyramid Bobber", photo: "/images/bobbers/pyramid_bobber.png", price: "$50,000", location: "Twilight Realm", luck: "+50", strength: "+10", expertise: "-", attractionRate: "+10%", bigCatchRate: "+10" },
-    { name: "Undying Heart", photo: "/images/bobbers/undying_heart.png", price: "$0", location: "Complete the Quest: Undying Love", luck: "+50", strength: "+20", expertise: "+20", attractionRate: "+10%", bigCatchRate: "+5" }
+    { name: "Basic Bobber", price: "$0", costNum: 0, location: "Default Bobber", luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Paulie's Bobber", price: "$0", costNum: 0, location: "Quest: Lost Saw", luck: "-", strength: "-", expertise: "+5", attractionRate: "+5%", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Blue Bobber", price: "$100", costNum: 100, location: "Coconut Bay", luck: "+5", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Dud Bobber", price: "$1,000", costNum: 1000, location: "Crescent Isle", luck: "+5", strength: "-", expertise: "+5", attractionRate: "-", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Feline Bobber", price: "$2,000", costNum: 2000, location: "Tanglewood", luck: "+5", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "+10", maxWeight: "-" },
+    { name: "Ornamental Bobber", price: "$10,000", costNum: 10000, location: "Luxian Dunes", luck: "+10", strength: "+5", expertise: "-", attractionRate: "+10%", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Lucky Bobber", price: "$10,000", costNum: 10000, location: "Luxian Dunes", luck: "+40", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "-" },
+    { name: "Rainbow Slime Bobber", price: "$35,000", costNum: 35000, location: "Vlad (Islander)", luck: "+30", strength: "+10", expertise: "-", attractionRate: "+10%", bigCatchRate: "+10", maxWeight: "-" },
+    { name: "Pyramid Bobber", price: "$50,000", costNum: 50000, location: "Twilight Realm", luck: "+50", strength: "+10", expertise: "-", attractionRate: "+10%", bigCatchRate: "+10", maxWeight: "-" },
+    { name: "Undying Heart", price: "$0", costNum: 0, location: "Quest: Undying Love", luck: "+50", strength: "+20", expertise: "+20", attractionRate: "+10%", bigCatchRate: "+5", maxWeight: "-" }
+];
+
+export const enchantmentsData: EnchantmentItem[] = [
+    { name: "Sin Encantamiento", rarity: "None", costNum: 0, luck: "0", strength: "0", expertise: "0", attractionRate: "0", bigCatchRate: "0", maxWeight: "0", description: "Ranura de encantamiento vacía." },
+    { name: "Lucky", rarity: "Common", costNum: 0, luck: "+15", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "-", description: "Aumento ligero de suerte." },
+    { name: "Reinforced", rarity: "Common", costNum: 0, luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "+400kg", description: "Permite capturar peces medianos." },
+    { name: "Speedy", rarity: "Common", costNum: 0, luck: "-", strength: "-", expertise: "-", attractionRate: "+10%", bigCatchRate: "-", maxWeight: "-", description: "Reduce el tiempo de espera." },
+    { name: "Day Walker", rarity: "Uncommon", costNum: 0, luck: "+50", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "-", description: "Bono notable de suerte diurna." },
+    { name: "Tubby Chaser", rarity: "Uncommon", costNum: 0, luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "+5", maxWeight: "+1,000kg", description: "Enfocado en ejemplares pesados." },
+    { name: "Light-Speed Reels", rarity: "Rare", costNum: 0, luck: "-", strength: "-", expertise: "-", attractionRate: "+40%", bigCatchRate: "-", maxWeight: "-", description: "Gran aceleración de capturas." },
+    { name: "Notoriously Big", rarity: "Rare", costNum: 0, luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "+10", maxWeight: "+50,000kg", description: "Capacidad para monstruos marinos." },
+    { name: "BIG BOYS ONLY", rarity: "Epic", costNum: 0, luck: "-", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "+65", maxWeight: "+100,000kg", description: "Especializado en tamaños colosales." },
+    { name: "Master of Balance", rarity: "Epic", costNum: 0, luck: "+20", strength: "+20", expertise: "+20", attractionRate: "+20%", bigCatchRate: "+20", maxWeight: "+400kg", description: "Bonificación simétrica a todas las estadísticas." },
+    { name: "Shiny Hunter", rarity: "Epic", costNum: 0, luck: "+80", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "-", description: "Farmeo de mutaciones Shiny." },
+    { name: "Speed Demon", rarity: "Epic", costNum: 0, luck: "-", strength: "-", expertise: "-", attractionRate: "+60%", bigCatchRate: "-", maxWeight: "-", description: "Elimina casi todo el tiempo de picada." },
+    { name: "God's Own Luck", rarity: "Legendary", costNum: 0, luck: "+250", strength: "-", expertise: "-", attractionRate: "-", bigCatchRate: "-", maxWeight: "-", description: "El mayor multiplicador de suerte pura." },
+    { name: "Messenger of the Heavens", rarity: "Legendary", costNum: 0, luck: "-", strength: "-", expertise: "-", attractionRate: "+100%", bigCatchRate: "-", maxWeight: "-", description: "Picada instantánea (0s de espera)." },
+    { name: "Strongest Angler", rarity: "Legendary", costNum: 0, luck: "+20", strength: "+85", expertise: "+85", attractionRate: "+10%", bigCatchRate: "+20", maxWeight: "+1,000,000kg", description: "Máxima facilidad en el minijuego de recogida." }
 ];
